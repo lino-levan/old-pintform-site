cd08c195962220c20f04e358b1d83fb83ce86f96
Lino Le Van <linolevan@Linos-MacBook-Air.local>
2021-11-03 10:18:19 -0700

    fixed demo and enabled log in buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,8 +13,8 @@ function Header() {
                 <div><NavLink exact={true} activeClassName='active-nav-link' to="/construction" className='nav-link'>Solutions</NavLink></div>
                 <div><NavLink exact={true} activeClassName='active-nav-link' to="/demo" className='nav-link'>Demo</NavLink></div>
                 <div><NavLink exact={true} activeClassName='active-nav-link' to="/pricing" className='nav-link'>Pricing</NavLink></div>
-                <div><Link to="/construction" className="button-filled">Log In</Link></div>
-                <div><Link to="/construction" className="button">Sign Up</Link></div>
+                <div><a href="https://webapp.pintform.com/login" className="button-filled">Log In</a></div>
+                <div><a href="https://webapp.pintform.com/login" className="button">Sign Up</a></div>
             </div>
         </div>
     );
--- src/pages/demo.js
+++ src/pages/demo.js
@@ -12,10 +12,10 @@ function Demo() {
             <section className='section' style={{backgroundColor:'var(--highlight-color)'}}>
                 <div>
                     <div className='center'>
-                        <h1>Communism</h1>
+                        <h1>User Satisfaction Survey</h1>
                     </div>
                     <div className='center'>
-                        <iframe title='Communism Form' src='https://forms.pintform.com/communism' style={{width: '400px', height:'700px', border: '0px',boxShadow: 'rgba(0, 0, 0, 0.08) 0px 2px 4px 0px, rgba(0, 0, 0, 0.06) 0px 2px 12px 0px', borderRadius: '10px'}}></iframe>
+                        <iframe title='Communism Form' src='https://forms.pintform.com/satisfaction' style={{width: '800px', height:'700px', border: '0px',boxShadow: 'rgba(0, 0, 0, 0.08) 0px 2px 4px 0px, rgba(0, 0, 0, 0.06) 0px 2px 12px 0px', borderRadius: '10px'}}></iframe>
                     </div>
                 </div>
             </section>
